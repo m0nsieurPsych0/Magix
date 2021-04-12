b5e90dfaa50a30be87cf75e7cba54a4e3504ac8d
m0nsieurPsych0 <thevilpsycho666@protonmail.com>
2021-04-12 17:26:33 -0400

    Ajout de home.php

---
+++ home.php
@@ -0,0 +1,11 @@
+<?php
+    require_once("action/LoginAction.php");
+
+    $action = new HomeAction();
+    $data = $action->execute();
+
+    require_once("partial/header.php");
+?>
+            <main>
+                
+            </main>
