c546792128dfd98e28cbda33b72932e0f628d20c
m0nsieurPsych0 <thevilpsycho666@protonmail.com>
2021-05-03 00:23:48 -0400

    Ajout des icones font awesome localement. Ménage des assets.

--- partial/header.php
+++ partial/header.php
@@ -6,7 +6,7 @@
 			<meta name="viewport" content="width=device-width, initial-scale=1">	
 			<script src="js/screenSaver.js"></script>
 			<link href="css/global.css" rel="stylesheet" />
-			<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+			<link rel="stylesheet" href="asset\fontawesome-free\css\all.css">
 			
 			<!-- Specific -->
 			<?php
