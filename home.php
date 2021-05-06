c65366eb070532c39c46bcd84bfb98f319927ad6
m0nsieurPsych0 <thevilpsycho666@protonmail.com>
2021-05-05 23:44:10 -0400

    Changement du onclick pour les items de la page home: au lieu d'être seulement le bouton qui est activé lors d'un click c'est le container au complet comme la surbrillance

--- home.php
+++ home.php
@@ -16,11 +16,11 @@
                     <div class="menu-title-wrapper">
                         <div class="menu-title">-=≡ MENU ≡=-</div>
                     </div>
-                    <div class="button-wrapper"><button id="jouer" onclick="window.location.href='?pvp=true';">__Jouer</button></div>
-                    <div class="button-wrapper"><button id="pratiquer" onclick="window.location.href='?pve=true';">__Pratiquer</button></div>
-                    <div class="button-wrapper"><button id="guide" onclick="window.location.href='guide.php';">__Guide_Stratégique</button></div>
-                    <div class="button-wrapper"><button id="screensaver" onclick="window.location.href='index.php';">__Écran_de_Veille</button></div>
-                    <div class="button-wrapper"><button id="quitter" onclick="window.location.href='?logout=true';">__Quitter</button></div>
+                    <div class="button-wrapper" onclick="window.location.href='?pvp=true';"><button id="jouer" >__Jouer</button></div>
+                    <div class="button-wrapper" onclick="window.location.href='?pve=true';"><button id="pratiquer" >__Pratiquer</button></div>
+                    <div class="button-wrapper" onclick="window.location.href='guide.php';"><button id="guide" >__Guide_Stratégique</button></div>
+                    <div class="button-wrapper" onclick="window.location.href='index.php';"><button id="screensaver" >__Écran_de_Veille</button></div>
+                    <div class="button-wrapper" onclick="window.location.href='?logout=true';"><button id="quitter" >__Quitter</button></div>
                 </div>
                 <div class="chat">
                     <div class="chat-title">°º¤ø,¸¸,ø¤º°`°º¤ø,¸,ø¤°º¤ø,¸¸,ø¤º°`°º¤ø,¸¸,ø¤º°`°º¤ø,¸¸,ø¤º°`°º¤ø,¸¸,ø¤º°`°º¤ø,¸¸,ø¤º°`°º¤ø,¸¸,ø¤º°`°º¤ø,¸¸,ø¤º°`°º¤ø,¸</div>
