ccf235c06378f1bb66925668ea327bc9194dc957
m0nsieurPsych0 <thevilpsycho666@protonmail.com>
2022-06-03 07:34:16 -0400

    Extend Connection.php to be able to open connection to any databases when called.

--- action/DAO/Connection.php
+++ action/DAO/Connection.php
@@ -3,12 +3,11 @@
     class Connection {
         private static $connection;
 
-        public static function getConnection() {
+        public static function getConnection($dbhost, $dbName, $dbuser, $dbpass) {
             if (empty(Connection::$connection)) {
-                Connection::$connection = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS);
+                Connection::$connection = new PDO("mysql:host=" . $dbhost . ";dbname=" . $dbName, $dbuser, $dbpass);
                 Connection::$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                 Connection::$connection->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-               
             }
 
             return Connection::$connection;
